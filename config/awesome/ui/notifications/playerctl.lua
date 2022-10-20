de490cf758fa1e686f7d2e4523c909b3a7726075
Bendz2012 <mail@benjamin-reiss.de>
2022-10-20 12:04:53 +0200

    added the ability to disable music notifications

--- config/awesome/ui/notifications/playerctl.lua
+++ config/awesome/ui/notifications/playerctl.lua
@@ -1,13 +1,15 @@
 local naughty = require("naughty")
 local playerctl_daemon = require("signal.playerctl")
 
-playerctl_daemon:connect_signal("metadata", function(_, title, artist, album_path, album, new, player_name)
-	if new == true then
-		naughty.notify({
-			app_name = "Music",
-			title = title,
-			text = artist,
-			image = album_path,
-		})
-	end
-end)
+if ({{@@ enable_music_notification @@}}) then
+	playerctl_daemon:connect_signal("metadata", function(_, title, artist, album_path, album, new, player_name)
+		if new == true then
+			naughty.notify({
+				app_name = "Music",
+				title = title,
+				text = artist,
+				image = album_path,
+			})
+		end
+	end)
+end
